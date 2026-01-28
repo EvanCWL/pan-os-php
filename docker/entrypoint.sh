b5ac25917eb47be80c10e7564925ecf25bb65b66
Evan Chua <evan.chua@paynet.my>
2026-01-28 14:39:29 +0800

    test claude fix for dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-# If no arguments are passed to the container, execute a bash shell, else execute
-# pan-os-php and pass the arguments to it 
-
-if [[ -z $@ ]]
-then
-    exec /bin/bash
+# Check if running in API mode
+if [ "$MODE" = "api" ]; then
+    echo "Starting Apache in API mode..."
+    # Start Apache in foreground
+    source /etc/apache2/envvars
+    exec apache2 -D FOREGROUND
 else
-    exec php -r "require_once('/tools/pan-os-php/utils/pan-os-php.php');" "$@"
+    # CLI mode (original behavior)
+    # If no arguments are passed to the container, execute a bash shell, else execute
+    # pan-os-php and pass the arguments to it 
+    
+    if [[ -z $@ ]]
+    then
+        exec /bin/bash
+    else
+        exec php -r "require_once('/tools/pan-os-php/utils/pan-os-php.php');" "$@"
+    fi
 fi
