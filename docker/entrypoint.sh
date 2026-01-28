30ac0e6522c2eaf9e65ced4241820b8bff9b5994
Evan Chua <evan.chua@paynet.my>
2026-01-28 15:01:48 +0800

    revert

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,11 @@
-#!/bin/bash
+#! /bin/bash
 
-# Check if running in API mode
-if [ "$MODE" = "api" ]; then
-    echo "Starting Apache in API mode..."
-    # Start Apache in foreground
-    source /etc/apache2/envvars
-    exec apache2 -D FOREGROUND
+# If no arguments are passed to the container, execute a bash shell, else execute
+# pan-os-php and pass the arguments to it 
+
+if [[ -z $@ ]]
+then
+    exec /bin/bash
 else
-    # CLI mode (original behavior)
-    # If no arguments are passed to the container, execute a bash shell, else execute
-    # pan-os-php and pass the arguments to it 
-    
-    if [[ -z $@ ]]
-    then
-        exec /bin/bash
-    else
-        exec php -r "require_once('/tools/pan-os-php/utils/pan-os-php.php');" "$@"
-    fi
+    exec php -r "require_once('/tools/pan-os-php/utils/pan-os-php.php');" "$@"
 fi
